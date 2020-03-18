235c364b895f8105a62f185bf9cf82535dbf9fab
rajeshgautam8 <rajeshgautam128@gmail.com>
2020-03-18 17:31:12 +0545

    simple layout done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'antd/dist/antd.css';
+import { Typography, Layout, Avatar, Menu,Breadcrumb   } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import './App.css';
 
+const { Header, Footer, Sider, Content } = Layout;
+const { Title } = Typography;
+const { SubMenu } = Menu;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{padding:'10px'}}>
+        <Avatar style={{float:'right'}} icon={<UserOutlined />} />
+          <Title level={3} style={{color:'#fff'}}>Rajesh</Title>
+        </Header>
+        <Layout>
+          <Sider >
+            <Menu defaultSelectedKeys = {['DashBoard']} mode="inline">
+            <Menu.Item key="DashBoard"> DashBoard </Menu.Item>
+              <SubMenu title = "Contact">
+                <Menu.ItemGroup key = 'aboutUs' title = "About us">
+                  <Menu.Item key = 'contact1'>Phone</Menu.Item>
+                  <Menu.Item key = 'contact2'>Email</Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+              <SubMenu title = "Branches">
+                <Menu.ItemGroup title = "Location">
+                  <Menu.Item key = 'branch1'>Kathmandu</Menu.Item>
+                  <Menu.Item key = 'branch2'>Lalitpur</Menu.Item>
+                  <Menu.Item key = 'branch3'>Bhaktapur</Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content style={{ padding: '0 50px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+                  <Breadcrumb.Item>Home</Breadcrumb.Item>
+                  <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="site-layout-content">Content</div>
+            </Content>
+          </Layout>
+        </Layout>
+        <Footer style={{ textAlign: 'center' }}>Ant Design Created by Rajesh</Footer>
+      </Layout>  
+      
     </div>
   );
 }
